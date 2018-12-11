cb40260be0280dde95c44b13132110c3b4bb1f8e
cleverTon <35335846+desenvolvedorIcotrade@users.noreply.github.com>
2018-12-11 15:15:14 -0200

    Create min.js

---
+++ min.js
@@ -0,0 +1,8 @@
+
+<div>
+< Roteiro src = "https://authedmine.com/lib/simple-ui.min.js" assíncronos > < / script de > 
+< div class = "coinhive-mineiro"  
+    estilo = "width: 256px; height: 310px" 
+    dados - key = "mzxesiqqER0JgrIC7nwQFoHKnZysrGPE " > 
+    < em > Carregando ... </ em > 
+</ div >
